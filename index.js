594cb1b1ef70920887713f3d708f860970ac82c0
saharazizighannad <52985223+saharazizighannad@users.noreply.github.com>
2019-07-17 17:03:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ let algorithm;
 
 if (config.algorithm === 'fifo') {
 	algorithm  = require('./lib/fifo').fifoFn;
-} else if (config.algorithm === 'roundRobin') {
-	algorithm  = require('./lib/roundRobin').roundRobinFn;
-} else if (config.algorithm === 'balancedRoundRobin') {
-	algorithm  = require('./lib/balancedRoundRobin').balancedRoundRobinFn;
 }
 const app = () => algorithm()
 
